ebac908ede627e1a62a8baea426b92089612cdf8
Your Name <you@example.com>
2024-04-08 00:47:25 -0400

    Primer commit, creacion de proyecto, ingreso de titulo, mas elementos de contacto

---
+++ app/struct/body/body.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<div class="container text-center">
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            Este es un portafolio profecional
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+</div>
